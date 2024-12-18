331cb57ba09834f0053bb298aadf8874f07a0ecb
Matthew <58734642+mwwalrath@users.noreply.github.com>
2024-12-18 13:57:11 -0500

    Create update_password.py

---
+++ Update Password/update_password.py
@@ -0,0 +1,22 @@
+import http.client
+import json
+import ssl
+
+eh_host = 'string'
+api_key = 'string'
+password = 'string'
+
+try:
+    connection = http.client.HTTPSConnection(eh_host, 443, context=ssl._create_unverified_context())
+    headers = {'accept': 'application/json', 'Authorization': f'ExtraHop apikey={api_key}', 'Content-Type': 'application/json'}
+    payload = {'name': 'setup','password': password}
+    connection.request('PATCH', '/api/v1/users', headers=headers,payload=json.dumps(payload))
+    response = connection.getresponse()
+    if response.status == 204:
+        print(f'{response.status}: User successfully updated.')
+    elif response.status == 401:
+        print.error(f'{response.status}: API key is missing or invalid.')
+    elif response.status == 402:
+        print.error(f'{response.status}: The EULA has not been accepted for this appliance.')
+except Exception as e:
+    print(f'Exception occured while retrieving users: {e}')
